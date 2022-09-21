4d36d5fc6a0515ebff6c0c4c78fed2f106153f1b
ryan foley <ryandoesdata@gmail.com>
2022-09-21 17:25:12 -0400

    tip calc exercise

--- Easy1/roomSize.js
+++ Easy1/roomSize.js
@@ -3,12 +3,9 @@ let rlSync = require('readline-sync');
 let length = rlSync.question('Enter the length\n');
 let width = rlSync.question('Enter the width\n');
 
-let sqrMeter = length * width;
+let sqrMeter = Math.floor(length * width);
 let sqrFeet = Math.floor(sqrMeter * 10.7639);
 
 
 console.log(`The area of the room is ${sqrMeter} square meters (${sqrFeet} 
 square feet).`)
-
-//let length = Number(rlSync.question('Enter the length of the room in 
-//meters:\n'));
