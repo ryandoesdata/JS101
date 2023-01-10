cc556a4fbd9ff79a387691d0bd7b10c829fa7009
ryan foley <ryandoesdata@gmail.com>
2023-01-10 12:06:12 -0500

    big progress

--- lesson_6/test.js
+++ lesson_6/test.js
@@ -1,52 +1,98 @@
+const readline = require('readline-sync');
+
+function prompt(msg) {
+  console.log(`=> ${msg}`);
+}
+
 // initialize deck
 
 let deck = [
-  {2: 2}, {2: 2}, {2: 2}, {2: 2}, {3: 3}, {3: 3}, {3: 3}, {3: 3},
-  {4: 4}, {4: 4}, {4: 4}, {4: 4}, {5: 5}, {5: 5}, {5: 5}, {5: 5}, {6: 6},
-  {6: 6}, {6: 6}, {6: 6}, {7: 7}, {7: 7}, {7: 7}, {7: 7}, {8: 8}, {8: 8},
-  {8: 8}, {8: 8}, {9: 9}, {9: 9}, {9: 9}, {9: 9}, {J: 10}, {J: 10}, {J: 10},
-  {J: 10}, {Q: 10}, {Q: 10}, {Q: 10}, {Q: 10}, {K: 10}, {K: 10},
-  {K: 10}, {K: 10}, {A: 10}, {A: 10}, {A: 10}, {A: 10}
+  {2: 2}, {2: 2}, {2: 2}, {2: 2},
+  {3: 3}, {3: 3}, {3: 3}, {3: 3},
+  {4: 4}, {4: 4}, {4: 4}, {4: 4},
+  {5: 5}, {5: 5}, {5: 5}, {5: 5},
+  {6: 6}, {6: 6}, {6: 6}, {6: 6},
+  {7: 7}, {7: 7}, {7: 7}, {7: 7},
+  {8: 8}, {8: 8}, {8: 8}, {8: 8},
+  {9: 9}, {9: 9}, {9: 9}, {9: 9},
+  {10: 10}, {10: 10}, {10: 10}, {10: 10},
+  {J: 10}, {J: 10}, {J: 10}, {J: 10},
+  {Q: 10}, {Q: 10}, {Q: 10}, {Q: 10},
+  {K: 10}, {K: 10}, {K: 10}, {K: 10},
+  {A: 10}, {A: 10}, {A: 10}, {A: 10}
 ];
+
 // deal hands
 
 let player = [];
 let dealer = [];
 
-function dealCards (deck) {
-
-  player.push(deck.splice(Math.floor(Math.random() * deck.length), 1));
-
-  player = player.flat();
-
-  player.push(deck.splice(Math.floor(Math.random() * deck.length), 1));
-
-  player = player.flat();
-
-  dealer.push(deck.splice(Math.floor(Math.random() * deck.length), 1));
-
-  dealer = dealer.flat();
-
-  dealer.push(deck.splice(Math.floor(Math.random() * deck.length), 1));
-
-  dealer = dealer.flat();
+function dealCards (deck, whoGoes, cards) {
+  let i = 0;
+  while (i < cards) {
+    whoGoes.push(deck.splice(Math.floor(Math.random() * deck.length), 1));
+    whoGoes = whoGoes.flat();
+    i++;
+  }
+  return whoGoes;
 }
 
 let playerHand = [];
 let dealerHand = [];
 
-function displayCards (whoGoes, whoseHand) {
-
+function getHand (whoGoes, whoseHand) {
   for (let i = 0; i < whoGoes.length; i++) {
-    whoseHand.push(Object.values(whoGoes[i]));
+    whoseHand.push(Number(Object.values(whoGoes[i])));
   }
-  console.log(whoseHand.flat());
+  whoseHand = whoseHand.flat();
+  //console.log(whoseHand);
 }
 
-dealCards(deck);
+function hit (whoGoes, whoseHand) {
+  whoseHand.push(Number(Object.values(whoGoes[whoGoes.length - 1])));
+  whoseHand = whoseHand.flat();
+  //console.log(whoseHand);
+}
 
-displayCards(player, playerHand);
+function addCards (hand) {
+  let pointTotal =
+  hand.reduce((accumulator, currentValue) => accumulator + currentValue
+  );
+  return pointTotal;
+}
 
-displayCards(dealer, dealerHand);
+player = dealCards(deck, player, 2);
+dealer = dealCards(deck, dealer, 2);
 
-// Get hands to numbers
+getHand(player, playerHand);
+getHand(dealer, dealerHand);
+
+function displayHand (who, whoseHand) {
+  console.log(`${who}'s hand: ${whoseHand.join(', ')}`);
+}
+
+displayHand('Player', playerHand);
+displayHand ('Dealer', dealerHand);
+
+let playerTotal = addCards(playerHand);
+let dealerTotal = addCards(dealerHand);
+
+while (true) {
+  prompt(`You have ${playerTotal}. Dealer has ${dealerTotal}.`);
+  prompt('Would you like to hit or stay?');
+  let playerMove = readline.question().trim().toLowerCase();
+  if (playerMove === "hit") {
+    player = dealCards(deck, player, 1);
+    dealer = dealCards(deck, dealer, 1);
+    hit(player, playerHand);
+    hit(dealer, dealerHand);
+    displayHand ('Player', playerHand);
+    displayHand ('Dealer', dealerHand);
+    playerTotal = addCards(playerHand);
+    dealerTotal = addCards(dealerHand);
+  } else if (playerMove === "stay") {
+    console.log(playerHand);
+    console.log(dealerHand);
+    break;
+  }
+}
