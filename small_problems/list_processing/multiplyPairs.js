012678530ca65d21cd635694e20cb5ca753e7b91
ryan foley <ryandoesdata@gmail.com>
2023-05-26 12:33:07 -0400

    cleanup

--- small_problems/list_processing/multiplyPairs.js
+++ small_problems/list_processing/multiplyPairs.js
@@ -1,17 +1,13 @@
-function multiplyAllPairs(arrOne, arrTwo) {
-  let arrThree = [];
-  let idxOne = 0;
-  let idxTwo = 0;
+/* eslint-disable id-length */
+function multiplyAllPairs (arr1, arr2) {
+  let arr3 = [];
 
-  while (idxOne < arrOne.length) {
-    while (idxTwo < arrTwo.length) {
-      arrThree.push(arrOne[idxOne] * arrTwo[idxTwo]);
-      idxTwo += 1;
+  for (let i = 0; i < arr1.length; i += 1) {
+    for (let j = 0; j < arr2.length; j += 1) {
+      arr3.push(arr1[i] * arr2[j]);
     }
-    idxOne += 1;
-    idxTwo = 0;
   }
-  return arrThree.sort((a, b) => a - b);
+  return arr3.sort((a, b) => a - b);
 }
 
 console.log(multiplyAllPairs([2, 4], [4, 3, 1, 2]));    // [2, 4, 4, 6, 8, 8, 12, 16])
--- test.js
+++ test.js
@@ -1,5 +1,22 @@
-let sum = num => String(num).split('').reduce((a, b) => a + Number(b), 0);
+function substrings (string) {
+  let arr = [];
 
-sum(23);           // 5
-sum(496);          // 19
-sum(123456789);    // 45
+  for (let i = 0; i <= string.length; i += 1) {
+    for (let j = i + 1; j <= string.length; j += 1) {
+      arr.push(string.substring(i, j));
+    }
+  }
+  return arr;
+}
+
+// j starts at 1 each time. Needs to increment by 1
+
+
+substrings('abcde');
+
+// returns
+[ "a", "ab", "abc", "abcd", "abcde",
+  "b", "bc", "bcd", "bcde",
+  "c", "cd", "cde",
+  "d", "de",
+  "e" ]
